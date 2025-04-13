7e49df2c2f6d3b30b449465e45726f3b71f151e3
Kayhub2 <feelingkay10@gmail.com>
2025-04-12 22:04:50 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+var typed = new Typed(".typing", {
+    strings: [ " ", "Web Designer.", "Software Developer.", "Classroom Teacher.", "Trainer."],
+    typeSpeed:120,
+    BackSpeed: 100,
+    loop: true
+})
